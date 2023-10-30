5736354fc4fcc75a8f0fc53e88db7903d9813a57
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-10-31 01:16:51 +0600

    log in page and sign up page design finished

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
 
-
-const Login = () => {
-  const handleLogin = (e) => {
+const SignUp = () => {
+  const handleSignup = (e) => {
     e.preventDefault();
-    
-  }
-  
+  };
 
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -17,8 +14,19 @@ const Login = () => {
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
-            <h1 className="text-3xl text-center font-bold">Login</h1>
-            <form onSubmit={handleLogin}>
+            <h1 className="text-3xl text-center font-bold">Sign Up Now</h1>
+            <form onSubmit={handleSignup}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="Name"
+                  placeholder="name"
+                  className="input input-bordered"
+                />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -67,4 +75,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 const Router = createBrowserRouter([
   {
@@ -9,15 +10,18 @@ const Router = createBrowserRouter([
     element: <MainLayout></MainLayout>,
     children: [
       {
-        path: '/',
-        element:<Home></Home>
+        path: "/",
+        element: <Home></Home>,
       },
       {
-        path: '/login',
-        element:<Login></Login>
-      }
-
-    ]
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+    ],
   },
 ]);
 
